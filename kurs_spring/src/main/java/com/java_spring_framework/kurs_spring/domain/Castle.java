999d7acb9af44f04d33bcaeb399523b17407da2f
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-02-19 23:21:13 +0100

    Kurs Spring Framework - Cykl życia Spring Beans

--- kurs_spring/src/main/java/com/java_spring_framework/kurs_spring/domain/Castle.java
+++ kurs_spring/src/main/java/com/java_spring_framework/kurs_spring/domain/Castle.java
@@ -1,5 +1,7 @@
 package com.java_spring_framework.kurs_spring.domain;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,5 +12,13 @@ public class Castle {
     private String name = "East Watch";
 
     public Castle() {
+    }
+@PostConstruct
+    public void build() {
+        System.out.println("Wybudowano zamek " + name);
+    }
+@PreDestroy
+    public void tearDown() {
+        System.out.println("Zaraz wyburzymy zamek " + name);
     }
 }
