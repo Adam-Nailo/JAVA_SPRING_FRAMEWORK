095166de6b6249db1f6607377fc9dbaa6ed994d4
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-02-19 23:08:41 +0100

    Kurs Spring Framework - Spring Beans, Kontekst, Kontener

---
+++ kurs_spring/src/main/java/com/java_spring_framework/kurs_spring/domain/Castle.java
@@ -0,0 +1,14 @@
+package com.java_spring_framework.kurs_spring.domain;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Adam Seweryn
+ */
+@Component
+public class Castle {
+    private String name = "East Watch";
+
+    public Castle() {
+    }
+}
