1644ba172f69f0f6fedba6afeea70174ec273d41
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-02-26 00:02:18 +0100

    Kurs Spring Framework - Zaawansowane DI - Wstrzykiwanie kolekcji

--- kurs_spring/src/main/java/com/java_spring_framework/kurs_spring/domain/Tournament.java
+++ kurs_spring/src/main/java/com/java_spring_framework/kurs_spring/domain/Tournament.java
@@ -4,26 +4,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 @Component
 public class Tournament {
 
-    @Autowired
-            @Qualifier(value = "percival")
-    Knight knight;
+//    @Autowired
+    Map<String,Knight> knights;
 
     public Tournament() {
     }
 
-    public void setKnight(Knight knight) {
-        this.knight = knight;
+    public void setKnights(Map<String, Knight> knights) {
+        this.knights = knights;
     }
 
-    public void duel(){
-        knight.setAge(knight.getAge()+1);
+    public void duel() {
+//        knight.setAge(knight.getAge() + 1);
     }
 
     @Override
     public String toString() {
-        return "W turnieju bierze udział rycerz: " + knight;
+        return "W turnieju biorą udział rycerze: " + knights.stream().map(Object::toString).collect(Collectors.joining(","));
     }
 }
