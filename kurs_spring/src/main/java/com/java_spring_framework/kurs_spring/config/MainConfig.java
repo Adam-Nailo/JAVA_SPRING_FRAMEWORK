14330260c00b1b8a8fe92b3c777fde766aa6cd26
Adam <155159635+Adam-Nailo@users.noreply.github.com>
2024-03-12 22:06:42 +0100

    Kurs Spring Framework - Podstawy Spring Security - Dodanie Spring Security do projektu

--- kurs_spring/src/main/java/com/java_spring_framework/kurs_spring/config/MainConfig.java
+++ kurs_spring/src/main/java/com/java_spring_framework/kurs_spring/config/MainConfig.java
@@ -3,7 +3,9 @@ package com.java_spring_framework.kurs_spring.config;
 import com.java_spring_framework.kurs_spring.domain.repository.DBKnightRepository;
 import com.java_spring_framework.kurs_spring.domain.repository.InMemoryRepository;
 import com.java_spring_framework.kurs_spring.domain.repository.KnightRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.*;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 
 @Configuration
 public class MainConfig {
@@ -20,6 +22,14 @@ public class MainConfig {
     public KnightRepository createDBKnightRepo(){
         KnightRepository repo = new DBKnightRepository();
         return repo;
+    }
+
+    @Autowired
+    public void securityUsers(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.inMemoryAuthentication()
+                .withUser("user1").password("{noop}user1").roles("USER")
+                .and()
+                .withUser("user2").password("{noop}user2").roles("ADMIN");
     }
 
 }
